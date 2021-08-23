f16f6bbc9617a656f7c9b8b45bb947c0d1ed2185
Konstantin Myrsaitov <myrsaitov@gmail.com>
2021-08-24 00:45:55 +0300

    fixed

--- Advertisement/Application/Advertisement/Contracts/Exceptions/AdvertisementNotFoundException.cs
+++ Advertisement/Application/Advertisement/Contracts/Exceptions/AdvertisementNotFoundException.cs
@@ -2,10 +2,10 @@
 
 namespace sev1.Advertisements.Application.Advertisement.Contracts.Exceptions
 {
-    public sealed class ContentNotFoundException : NotFoundException
+    public sealed class AdvertisementNotFoundException : NotFoundException
     {
         private const string MessageTemplate = "Объявление с таким ID[{0}] не было найдено.";
-        public ContentNotFoundException(int contentId) : base(string.Format(MessageTemplate, contentId))
+        public AdvertisementNotFoundException(int contentId) : base(string.Format(MessageTemplate, contentId))
         {
         }
     }
