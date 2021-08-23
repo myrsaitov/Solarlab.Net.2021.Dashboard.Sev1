36d90cdcd5a3009d93450508282a908c7e8ddcb9
Konstantin Myrsaitov <myrsaitov@gmail.com>
2021-08-24 01:05:55 +0300

    fixes

---
+++ Advertisements/Application/Category/Contracts/Delete.cs
@@ -0,0 +1,10 @@
+﻿namespace sev1.Advertisements.Application.Advertisement.Contracts
+{
+    public static class Delete
+    {
+        public sealed class Request
+        {
+            public int Id { get; set; }
+        }
+    }
+}
---
+++ Advertisements/Application/Tag/Implementations/ContentServiceV1.Create.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sev1.Advertisements.Application.Advertisement.Implementations
+{
+    public sealed partial class AdvertisementServiceV1 : IAdvertisementService
+    {
+        public async Task<Create.Response> Create(
+            Create.Request request, 
+            CancellationToken cancellationToken)
+        {
+            if (request is null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            var category = await _categoryRepository.FindById(
+                request.CategoryId,
+                cancellationToken);
+
+            if (category is null)
+            {
+                throw new CategoryNotFoundException(request.CategoryId);
+            }
+
+            string userId = await _identityService.GetCurrentUserId(cancellationToken);
+
+            var content = _mapper.Map<Domain.Content>(request);
+            content.IsDeleted = false;
+            content.OwnerId = userId;
+            content.CreatedAt = DateTime.UtcNow;
+            content.Category = category;
+
+            if (request.TagBodies is not null)
+            {
+                content.Tags = new List<Domain.Tag>();
+                foreach (string body in request.TagBodies)
+                {
+                    if (body.Length > 0)
+                    {
+                        var tag = await _tagRepository.FindWhere(
+                            a => a.Body == body,
+                            cancellationToken);
+
+                        if (tag == null)
+                        {
+                            var tagRequest = new Tag.Contracts.Create.Request()
+                            {
+                                Body = body
+                            };
+
+                            tag = _mapper.Map<Domain.Tag>(tagRequest);
+                            tag.CreatedAt = DateTime.UtcNow;
+                            tag.Count = 1;
+
+                            await _tagRepository.Save(
+                                tag, 
+                                cancellationToken);
+                        }
+                        else
+                        {
+                            // TODO Переделать с поиском в базе, учесть удаленные объявления
+                            tag.Count += 1;
+                            await _tagRepository.Save(tag, cancellationToken);
+                        }
+
+                        content.Tags.Add(tag);
+                    }
+                }
+            }
+
+            await _contentRepository.Save(
+                content, 
+                cancellationToken);
+
+            return new Create.Response
+            {
+                Id = content.Id
+            };
+        }
+    }
+}
