50c118082ba9ec71b4476275a239512ff0ad5e3f
Skarfe <gratskyiyurii@mail.ru>
2021-09-05 22:23:49 +0300

    #127 Сущьность комментария

---
+++ scr/CommentService/Domain/Entities/Comment.cs
@@ -0,0 +1,43 @@
+﻿using Contracts.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Сущьность коментария
+    /// </summary>
+    public class Comment
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        [Key]
+        public Guid Id { get; set; }
+        
+        /// <summary>
+        /// Текст сообщения
+        /// </summary>
+        public String Message { get; set; }
+
+        /// <summary>
+        /// Дата создания
+        /// </summary>
+        public DateTime CreationTime { get; set; }
+
+        /// <summary>
+        /// Статус сообщения
+        /// </summary>
+        public CommentStatus CommentStatus { get; set; }
+
+        /// <summary>
+        /// Id объявления, к которому прикреплён коментарий
+        /// </summary>
+        public Guid AdvertesmentId { get; set; }
+
+        /// <summary>
+        /// Id автора
+        /// </summary>
+        public Guid AuthorId { get; set; }
+    }
+}
