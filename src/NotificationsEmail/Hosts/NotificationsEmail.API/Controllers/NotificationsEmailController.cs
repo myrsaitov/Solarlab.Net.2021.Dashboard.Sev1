dfd0f13d616553f10ed68ed5f06e46b719b8b9c2
Skarfe <gratskyiyurii@mail.ru>
2021-10-11 18:52:52 +0300

    #107 API added

---
+++ src/NotificationsEmail/Hosts/NotificationsEmail.API/Controllers/NotificationsEmailController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NotificationsEmail.Contracts;
+using NotificationsEmail.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotificationsEmail.API.Controllers
+{
+    /// <summary>
+    /// API Сервиса нотификации по почте
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsEmailController : ControllerBase
+    {
+        private readonly ILogger<NotificationsEmailController> _logger;
+        private readonly INotificationEmailService _notificationEmailService;
+
+        public NotificationsEmailController(ILogger<NotificationsEmailController> logger, INotificationEmailService notificationEmailService)
+        {
+            _logger = logger;
+            _notificationEmailService = notificationEmailService;
+        }
+
+        /// <summary>
+        /// Отправить Email
+        /// </summary>
+        /// <param name="dto">Письмо</param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult SendEmail([FromForm] LetterDto dto)
+        {
+            if (ModelState.IsValid)
+            {
+                _notificationEmailService.SendEmailAsync(dto);
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
