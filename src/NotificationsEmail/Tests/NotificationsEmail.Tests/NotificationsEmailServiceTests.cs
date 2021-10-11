5a2765055dd29272696560544adc003fb858a5c5
Skarfe <gratskyiyurii@mail.ru>
2021-10-11 18:53:36 +0300

    Empty UnitTests project added

---
+++ src/NotificationsEmail/Tests/NotificationsEmail.Tests/NotificationsEmailServiceTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace NotificationsEmail.Tests
+{
+    public class NotificationsEmailServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
