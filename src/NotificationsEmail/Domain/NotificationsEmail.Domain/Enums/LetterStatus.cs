631f4dbae645e70749d684496c9bb56cbd31907d
Skarfe <gratskyiyurii@mail.ru>
2021-10-11 18:42:29 +0300

    Entity added

---
+++ src/NotificationsEmail/Domain/NotificationsEmail.Domain/Enums/LetterStatus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationsEmail.Domain.Enums
+{
+    /// <summary>
+    /// Статус отправки письма
+    /// </summary>
+    public enum LetterStatus
+    {
+        /// <summary>
+        /// 0 - Новое сообщение
+        /// </summary>
+        New,
+
+        /// <summary>
+        /// 1 - Попытка отправить сообщение
+        /// </summary>
+        Trying,
+
+        /// <summary>
+        /// 2 - Сообщение отправлено
+        /// </summary>
+        Sent,
+
+        /// <summary>
+        /// 3 - Произошла ошибка при отправки сообщения
+        /// </summary>
+        Failed
+    }
+}
