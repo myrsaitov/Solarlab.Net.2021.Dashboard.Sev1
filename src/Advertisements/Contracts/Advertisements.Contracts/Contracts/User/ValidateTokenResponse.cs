57d075b779403c3fb97022b9152076dd9152ae49
Konstantin Myrsaitov <myrsaitov@gmail.com>
2021-11-10 01:14:16 +0300

    Добавлены комментарии

--- src/Advertisements/Contracts/Advertisements.Contracts/Contracts/User/ValidateTokenResponse.cs
+++ src/Advertisements/Contracts/Advertisements.Contracts/Contracts/User/ValidateTokenResponse.cs
@@ -2,9 +2,19 @@
 
 namespace Sev1.Advertisements.Contracts.Contracts.User
 {
+    /// <summary>
+    /// DTO ответа на запрос валидации токена
+    /// </summary>
     public class ValidateTokenResponse
     {
+        /// <summary>
+        /// Id пользователя
+        /// </summary>
         public string UserId { get; set; }
+
+        /// <summary>
+        /// Список ролей
+        /// </summary>
         public IEnumerable<string> Roles { get; set; }
     }
 }
--- src/UserFiles/Contracts/UserFiles.Contracts/Contracts/YandexDisk/GetUploadUrlRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sev1.UserFiles.Contracts.Contracts.YandexDisk
-{
-    public class GetUploadUriRequest
-    {
-        public string Path { get; set; }
-    }
-}
