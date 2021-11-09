6396cd3f8ceabac2330f34265fcc829cbd2b0294
Konstantin Myrsaitov <myrsaitov@gmail.com>
2021-11-10 01:23:14 +0300

    Исправлены проблемы кодировки в комментариях

--- src/UserFiles/Hosts/UserFiles.Api/Program.cs
+++ src/UserFiles/Hosts/UserFiles.Api/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace Sev1.UserFiles.Api
 {
-    // ���� �������������, �������� � ����������� � ������ Program
+    // Хост настраивается, строится и запускается в классе Program
     public class Program
     {
         
@@ -12,15 +12,15 @@ namespace Sev1.UserFiles.Api
             CreateHostBuilder(args).Build().Run();
 
             // Run()
-            // ���� ����� ��������� middleware-��������� � ���� Run[Middleware],
-            // ������� ���������� � ����� ���������.��� �������,
-            // �� ��������� ��� ���������� middleware � �����������
-            // � ����� ��������� ��������, ��������� �� ����� ��������
-            // ��������� middleware - ���������.
+            // Этот метод добавляет middleware-компонент в виде Run[Middleware],
+            // который выполнится в конце конвейера.Как правило,
+            // он действует как замыкающее middleware и добавляется
+            // в конце конвейера запросов, поскольку не может вызывать
+            // следующий middleware - компонент.
             // https://habr.com/ru/company/otus/blog/528692/
         }
 
-        // ���������� ������������� ���������
+        // Абстракция инициализации программы
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
