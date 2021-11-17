5fb0fe1ec84c5519dcacb0e6553d03311ff422c0
Skarfe <gratskyiyurii@mail.ru>
2021-11-17 17:59:18 +0300

    Обновлены миграции

--- src/Comments/Infrastructure/CommentMigrations/Migrations/20211117111511_InitialCreate.Designer.cs
+++ src/Comments/Infrastructure/CommentMigrations/Migrations/20211117111511_InitialCreate.Designer.cs
@@ -4,14 +4,16 @@ using Comments.Repository.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Comments.Migrations.Migrations
 {
     [DbContext(typeof(CommentDBContext))]
-    partial class CommentDBContextModelSnapshot : ModelSnapshot
+    [Migration("20211117111511_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Comments/Infrastructure/CommentMigrations/Migrations/20211117111511_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Comments.Migrations.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Chat_Table",
+                columns: table => new
+                {
+                    ChatId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    AdvertisementId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ConsumerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SellerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chat_Table", x => x.ChatId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comment_Table",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CommentStatus = table.Column<int>(type: "int", nullable: false),
+                    IsUpdated = table.Column<bool>(type: "bit", nullable: false),
+                    ChatId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comment_Table", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comment_Table_Chat_Table_ChatId",
+                        column: x => x.ChatId,
+                        principalTable: "Chat_Table",
+                        principalColumn: "ChatId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_Table_ChatId",
+                table: "Comment_Table",
+                column: "ChatId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comment_Table");
+
+            migrationBuilder.DropTable(
+                name: "Chat_Table");
+        }
+    }
+}
