09bd3bd73b0a9fb4bd404a1ace4ea183f448d19a
Konstantin Myrsaitov <myrsaitov@gmail.com>
2022-01-18 01:58:56 +0300

    Добавлены файлы запуска Update-Database

---
+++ mssql_install_tools.cmd
@@ -0,0 +1,11 @@
+dotnet tool install -g dotnet-ef --version 3.0.0-preview4.19216.3
+
+pause
+
+dotnet tool update --global dotnet-ef --version 3.0.0-preview7.19362.6
+
+pause
+
+dotnet tool update --global dotnet-ef --version 5.0.11
+
+pause
