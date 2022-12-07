d329a22235aa34c3a5833c4cc02a77b83991da50
Konstantin Myrsaitov <myrsaitov@gmail.com>
2022-12-08 00:34:37 +0300

    Перемещены скрипты

---
+++ setup/docker_mssql/mssql_dotnet_tool_setup.cmd
@@ -0,0 +1,19 @@
+
+https://www.nuget.org/api/v2/package/dotnet-ef/5.0.11
+Install-Package dotnet-ef -Source dotnet-ef.5.0.11.nupkg
+
+dotnet --info
+
+dotnet tool uninstall dotnet-ef --global
+
+dotnet tool install -g dotnet-ef --version 3.0.0-preview4.19216.3
+
+PAUSE
+
+dotnet tool update --global dotnet-ef --version 3.0.0-preview7.19362.6
+
+PAUSE
+
+dotnet tool update --global dotnet-ef --version 5.0.11
+
+PAUSE
---
+++ setup/docker_mssql/mssql_trust.cmd
@@ -0,0 +1,3 @@
+dotnet dev-certs https --trust
+
+PAUSE
