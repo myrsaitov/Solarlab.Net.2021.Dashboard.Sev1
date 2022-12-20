f1adc3869231339852f02de417bf47c6bd5016d7
Konstantin Myrsaitov <myrsaitov@gmail.com>
2022-12-20 04:56:49 +0300

    Проблема сертификата решена

--- certificate_reset.cmd
+++ certificate_reset.cmd
@@ -3,9 +3,21 @@
 :: https://medium.com/@woeterman_94/docker-in-visual-studio-unable-to-configure-https-endpoint-f95727187f5f
 :: https://stackoverflow.com/questions/73628719/docker-net-web-api-dev-certs-error-the-ssl-connection-could-not-be-establishe
 :: https://www.niceonecode.com/question/20641/unable-to-configure-https-endpoint.-no-server-certificate-was-specified-and-the-default-developer-certif
+
+:: Помогло:
+:: Очистить все папки:
 :: C:\Users\user\AppData\Roaming/Microsoft/UserSecrets
 :: C:\Users\user\AppData\Roaming/ASP.NET/Https
-
+::
+:: Удалить все сертификаты:
+:: dotnet dev-certs https --clean
+:: RUN "certmgr.msc": Clean "localhost" in
+::   - Personal/Certificates
+::   - Current User Trusted Root Certification Authorities/Certificates
+:: 
+:: Очистить все бинарники у докера и в проекте
+:: 
+:: Запустить полностью чистый проект, подтвердить создание сертификата
 
 :: Устанавливает заголовок окна
 TITLE Run "dev-certs clean and set"
@@ -25,9 +37,7 @@ dotnet dev-certs https --trust
 
 PAUSE
 
-ECHO ALSO: RUN "certmgr.msc": Clean "localhost" in
-ECHO   - Personal/Certificates
-ECHO   - Current User Trusted Root Certification Authorities/Certificates
+
 ECHO Restart Visual Studio!!!
 
 PAUSE
