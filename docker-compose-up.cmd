56ea62c1c575ae251e1985957f2564e8b9fa3537
Konstantin Myrsaitov <myrsaitov@gmail.com>
2022-12-24 02:45:38 +0300

    Исправлены скрипты

--- docker-compose-up.cmd
+++ docker-compose-up.cmd
@@ -4,7 +4,56 @@
 :: Устанавливает заголовок окна
 TITLE Run "docker-compose UP"
 
+
+:: Создание Volumes
+ECHO;
+ECHO *****************************************
+ECHO * Creating Volumes
+ECHO *****************************************
+ECHO;
+
+:: Accounts
+IF NOT EXIST c:\Docker\Root\BulletingBoard\Postgres\Accounts (
+    docker volume create^
+        --opt type=none^
+        --opt device=/c/Docker/Root/BulletingBoard/Postgres/Accounts/largedb^
+        --opt o=bind
+)
+
+:: Advertisements
+IF NOT EXIST c:\Docker\Root\BulletingBoard\Postgres\Advertisements (
+    docker volume create^
+        --opt type=none^
+        --opt device=/c/Docker/Root/BulletingBoard/Postgres/Advertisements/largedb^
+        --opt o=bind
+)
+
+:: Userfiles
+IF NOT EXIST c:\Docker\Root\BulletingBoard\Postgres\UserFiles (
+    docker volume create^
+        --opt type=none^
+        --opt device=/c/Docker/Root/BulletingBoard/Postgres/UserFiles/largedb^
+        --opt o=bind
+)
+
+
+:: Проверка наличия сети "dev-network", если нет, то создает её
+ECHO;
+ECHO *****************************************
+ECHO * Checking network "dev-network"
+ECHO *****************************************
+ECHO;
+docker network inspect dev-network || (
+    docker network create -d bridge dev-network
+)
+
+
 :: Запускает docker-compose
+ECHO;
+ECHO *****************************************
+ECHO * Starting containers
+ECHO *****************************************
+ECHO;
 docker-compose up --detach
 
 PAUSE
--- postgres-docker-compose-up.cmd
+++ postgres-docker-compose-up.cmd
@@ -4,7 +4,45 @@
 :: Устанавливает заголовок окна
 TITLE Run "postgres-docker-compose UP"
 
+
+:: Создание Volumes
+ECHO;
+ECHO *****************************************
+ECHO * Creating Volumes
+ECHO *****************************************
+ECHO;
+
+:: Accounts
+IF NOT EXIST c:\Docker\Root\BulletingBoard\Postgres\Accounts (
+    docker volume create^
+        --opt type=none^
+        --opt device=/c/Docker/Root/BulletingBoard/Postgres/Accounts/largedb^
+        --opt o=bind
+)
+
+:: Advertisements
+IF NOT EXIST c:\Docker\Root\BulletingBoard\Postgres\Advertisements (
+    docker volume create^
+        --opt type=none^
+        --opt device=/c/Docker/Root/BulletingBoard/Postgres/Advertisements/largedb^
+        --opt o=bind
+)
+
+:: Userfiles
+IF NOT EXIST c:\Docker\Root\BulletingBoard\Postgres\UserFiles (
+    docker volume create^
+        --opt type=none^
+        --opt device=/c/Docker/Root/BulletingBoard/Postgres/UserFiles/largedb^
+        --opt o=bind
+)
+
+
 :: Запускает docker-compose
+ECHO;
+ECHO *****************************************
+ECHO * Starting containers
+ECHO *****************************************
+ECHO;
 docker-compose  --file postgres-docker-compose.yml up --detach
 
 PAUSE
