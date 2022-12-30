ce74eea432cfbc9315e1d01bfc3d1fdabbdd3f50
Konstantin Myrsaitov <myrsaitov@gmail.com>
2022-12-30 17:49:47 +0300

    UI новая версия, начало

--- src_ui/src/app/app.component.ts
+++ src_ui/src/app/app.component.ts
@@ -1,11 +1,10 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
-// The @Component decorator identifies the class immediately below it as a component class, and specifies its metadata.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dashboard2021-spa';
+  isCollapsed = false;
 }
