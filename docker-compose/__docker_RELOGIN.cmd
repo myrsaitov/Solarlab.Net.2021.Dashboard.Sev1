46a1a62a82ad6f57ad6cfd241f7425cb7bb8c968
Konstantin Myrsaitov <myrsaitov@gmail.com>
2023-01-10 19:30:04 +0300

    Исправлен docker-relogin

--- docker-compose/__docker_RELOGIN.cmd
+++ docker-compose/__docker_RELOGIN.cmd
@@ -1,5 +1,6 @@
 @ECHO OFF
 :: Выход и вход в докер
+:: https://docs.ansible.com/ansible/latest/collections/community/docker/docker_login_module.html
 :: https://www.dotnetforall.com/failed-to-solve-with-frontend-dockerfile-v0-failed-to-create-llb-definition-possible-solutions/
 
 :: Заголовок окна - путь и имя файла
